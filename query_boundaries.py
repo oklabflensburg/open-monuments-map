bae2001dc28690f28f5363cb0ae34bd453228a25
Aurelius Wendelken <webtobesocial@gmail.com>
2023-09-05 22:30:07 +0200

    add example how to fetch geojson

--- query_boundaries.py
+++ query_boundaries.py
@@ -3,39 +3,43 @@
 import os
 import json
 import psycopg2
+import click
 
-from geojson import FeatureCollection, MultiPolygon, Feature
-from os.path import join, dirname
+from geojson import FeatureCollection, GeometryCollection, MultiPolygon, Polygon, Feature
 from dotenv import load_dotenv
+from pathlib import Path
 
 
-dotenv_path = join(f'{dirname(__file__)}', '.env')
-load_dotenv(dotenv_path)  
-
-    
-conn = psycopg2.connect(    
-    database = os.getenv('DB_NAME'),
-    password = os.getenv('DB_PASS'),
-    user = os.getenv('DB_USER'),
-    host = os.getenv('DB_HOST'),
-    port = os.getenv('DB_PORT')
-)
+env_path = Path('.')/'.env'
+load_dotenv(dotenv_path=env_path)
 
 
-def get_boundaries(cur, objektid):
-    sql = 'SELECT ST_AsGeoJson(wkb_geometry) as geom FROM monument_boundaries WHERE objektid = %s'
+try:
+    conn = psycopg2.connect(
+        database = os.getenv('DB_NAME'),
+        password = os.getenv('DB_PASS'),
+        user = os.getenv('DB_USER'),
+        host = os.getenv('DB_HOST'),
+        port = os.getenv('DB_PORT')
+    )
+except Exception as e:
+    print(e)
 
-    multipolygon = []
-    cur.execute(sql, (objektid,))
-    row = cur.fetchone()
 
-    if row is not None:
-        multipolygon = json.loads(row[0])['coordinates']
+def get_boundaries(cur, object_id):
+    sql = 'SELECT ST_AsGeoJson(geometry) FROM monument_boundaries WHERE object_id = %s'
 
-    return multipolygon
+    boundaries = []
+
+    cur.execute(sql, (object_id,))
+    rows = cur.fetchall()
+
+    return rows
 
 
-def main():
+@click.command()
+@click.argument('object_id')
+def main(object_id):
     cur = conn.cursor()
 
     fc = []
@@ -47,12 +51,22 @@ def main():
         }
     }
 
-    for o in d:
-        multipolygon = MultiPolygon(get_boundaries(cur, o['object']))
+    boundaries = get_boundaries(cur, object_id)
+    properties = {'object_id': object_id}
 
-        fc.append(Feature(GeometryCollection([point, multipolygon]), properties=properties))
+    for boundary in boundaries:
+        row = json.loads(boundary[0])
+        coordinates = row['coordinates']
+
+        if row['type'].lower() == 'polygon':
+            shape = Polygon(coordinates)
+        elif row['type'].lower() == 'multipolygon':
+            shape = MultiPolygon(coordinates)
+
+        fc.append(Feature(geometry=shape, properties=properties))
 
     c = FeatureCollection(fc, crs=crs)
+    print(c)
 
 
 if __name__ == '__main__':
