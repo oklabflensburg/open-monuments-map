f653a3b341eb6f594c339cc88a0de39eb8acbfd3
Aurelius Wendelken <webtobesocial@gmail.com>
2023-09-07 00:32:36 +0200

    update postgresql insert statements

--- data/flensburg_denkmalschutz.sql
+++ data/flensburg_denkmalschutz.sql
@@ -2,14 +2,41 @@
 CREATE EXTENSION IF NOT EXISTS postgis;
 
 
-/* GEOMETRIE DENKMALSCHUTZUMFANG */
+
+
+/* TABELLE DENKMALOBJEKTE */
+DROP TABLE IF EXISTS monuments CASCADE;
+
+CREATE TABLE IF NOT EXISTS monuments (
+  id SERIAL,
+  object_id INT,
+  scope_id INT,
+  reason_id INT,
+  model VARCHAR,
+  address VARCHAR,
+  authority VARCHAR,
+  district VARCHAR,
+  image_url VARCHAR,
+  designation VARCHAR,
+  postal_code VARCHAR,
+  description TEXT,
+  geometry geography,
+  PRIMARY KEY(id)
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS monuments_object_id_idx ON monuments (object_id);
+
+
+
+/* TABELLE POLYGON DENKMALOBJEKT */
 DROP TABLE IF EXISTS monument_boundaries CASCADE;
 
 CREATE TABLE IF NOT EXISTS monument_boundaries (
   id SERIAL,
   object_id INT,
   geometry geography,
-  PRIMARY KEY(id)
+  PRIMARY KEY(id),
+  FOREIGN KEY(object_id) REFERENCES monuments(object_id)
 );
 
 CREATE UNIQUE INDEX IF NOT EXISTS monument_boundaries_object_id_idx ON monument_boundaries (object_id);
@@ -25,7 +52,19 @@ CREATE TABLE IF NOT EXISTS monument_scope (
   PRIMARY KEY(id)
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS monuments_scope_label_idx ON monument_reason (label);
+CREATE UNIQUE INDEX IF NOT EXISTS monuments_scope_label_idx ON monument_scope (label);
+
+
+
+/* TABELLE DENKMALOBJEKT SCHUTZUMFANG */
+DROP TABLE IF EXISTS monument_object_scope CASCADE;
+
+CREATE TABLE IF NOT EXISTS monument_object_scope (
+  object_id INT,
+  scope_id INT,
+  FOREIGN KEY(object_id) REFERENCES monuments(object_id),
+  FOREIGN KEY(scope_id) REFERENCES monument_scope(id)
+);
 
 
 
@@ -42,26 +81,12 @@ CREATE UNIQUE INDEX IF NOT EXISTS monuments_reason_label_idx ON monument_reason 
 
 
 
-/* TABELLE DENKMALOBJEKTE */
-DROP TABLE IF EXISTS monuments;
+/* TABELLE DENKMALOBJEKT BEGRÜNDUNG */
+DROP TABLE IF EXISTS monument_object_reason CASCADE;
 
-CREATE TABLE IF NOT EXISTS monuments (
-  id SERIAL,
+CREATE TABLE IF NOT EXISTS monument_object_reason (
   object_id INT,
-  scope_id INT,
   reason_id INT,
-  model VARCHAR,
-  address VARCHAR,
-  authority VARCHAR,
-  district VARCHAR,
-  image_url VARCHAR,
-  designation VARCHAR,
-  postal_code VARCHAR,
-  description TEXT,
-  geometry geography,
-  PRIMARY KEY(id),
-  FOREIGN KEY(scope_id) REFERENCES monument_scope(id),
+  FOREIGN KEY(object_id) REFERENCES monuments(object_id),
   FOREIGN KEY(reason_id) REFERENCES monument_reason(id)
 );
-
-CREATE UNIQUE INDEX IF NOT EXISTS monuments_object_id_idx ON monuments (object_id);
