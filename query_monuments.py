e0d1cb1a2abf2ba712e0c2510b68056ca0985899
Aurelius Wendelken <webtobesocial@gmail.com>
2023-09-12 11:17:25 +0200

    update script to query monuments by city

--- query_monuments.py
+++ query_monuments.py
@@ -28,39 +28,37 @@ except Exception as e:
 
 def get_monumets(cur, gen):
     sql = """
-        SELECT json_build_object(
+    SELECT
+    json_build_object(
+        'type', 'FeatureCollection',
+        'crs', json_build_object(
+            'type', 'name',
             'properties', json_build_object(
-                'object_id', m.object_id,
-                'place_name', m.place_name,
-                'address', m.address,
-                'postal_code', m.postal_code,
-                'image_url', m.image_url,
-                'designation', m.designation,
-                'description', m.description,
-                'monument_type', m.monument_type,
-                'reasons', string_agg(mr.label, ', ')
-            ), 'geometry', ST_ASGeoJson(ST_GeomFromEWKB(m.geometry))
+                'name', 'urn:ogc:def:crs:OGC:1.3:CRS84'
+            )
+        ),
+        'features', json_agg(
+            json_build_object(
+                'type', 'Feature',
+                'geometry', ST_AsGeoJSON(m.wkb_geometry)::json,
+                'properties', json_build_object(
+                    'object_id', m.object_id,
+                    'place_name', m.place_name,
+                    'address', m.address,
+                    'postal_code', m.postal_code,
+                    'image_url', m.image_url,
+                    'designation', m.designation,
+                    'description', m.description,
+                    'monument_type', m.monument_type,
+                    'reasons', (SELECT string_agg(mr.label, ', ') FROM monument_reason AS mr GROUP BY mxr.monument_id)
+                )
+            )
         )
-        FROM
-            monuments AS m
-        LEFT JOIN
-            monument_x_reason AS mxr ON mxr.monument_id = m.id
-        LEFT JOIN
-            monument_reason AS mr ON mxr.reason_id = mr.id
-        JOIN
-            vg250 AS v ON ST_Within(ST_GeomFromEWKB(m.geometry),
-            ST_GeomFromEWKB(v.wkb_geometry)) AND LOWER(v.gen) = %s
-        GROUP BY
-            m.object_id,
-            m.place_name,
-            m.address,
-            m.postal_code,
-            m.image_url,
-            m.designation,
-            m.description,
-            m.monument_type,
-            mxr.monument_id,
-            m.geometry
+    )
+    FROM monuments as m
+    JOIN monument_x_reason AS mxr ON mxr.monument_id = m.id
+    JOIN monument_reason AS mr ON mxr.reason_id = mr.id
+    JOIN vg250 AS v ON ST_Within(ST_GeomFromEWKB(m.wkb_geometry), ST_GeomFromEWKB(v.wkb_geometry)) WHERE LOWER(v.gen) = %s
     """
 
     monuments = []
@@ -76,28 +74,10 @@ def get_monumets(cur, gen):
 def main(gen):
     cur = conn.cursor()
 
-    fc = []
-
-    crs = {
-        'type': 'name',
-        'properties': {
-            'name': 'urn:ogc:def:crs:OGC:1.3:CRS84'
-        }
-    }
-
     monuments = get_monumets(cur, gen.lower())
 
-    for monument in monuments:
-        row = monument[0]
-        print(row)
-        properties = row['properties']
-        geometry = json.loads(row['geometry'])
-        print(geometry)
-        shape = Point(geometry['coordinates'])
-        fc.append(Feature(geometry=shape, properties=properties))
-
-    c = FeatureCollection(fc, crs=crs)
-    print(c)
+    with open(f'{gen}.geojson', 'w') as f:
+        json.dump(monuments[0][0], f)
 
 
 if __name__ == '__main__':
