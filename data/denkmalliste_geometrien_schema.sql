41c86f1b46c567d8fb92d12e5daaca18657f5acf
Aurelius Wendelken <webtobesocial@gmail.com>
2024-07-26 13:21:51 +0200

    added latest monument boundaries and schema

--- data/denkmalliste_geometrien_schema.sql
+++ data/denkmalliste_geometrien_schema.sql
@@ -1,5 +1,6 @@
--- POSTGIS ERWEITERUNG LADEN
+-- POSTGIS UND PGCRYPTO ERWEITERUNG LADEN
 CREATE EXTENSION IF NOT EXISTS postgis;
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 
 -- HILFSTABELLE GEOMETRIEN DENKMALLISTE
@@ -7,12 +8,13 @@ DROP TABLE IF EXISTS monument_boundaries CASCADE;
 
 CREATE TABLE IF NOT EXISTS monument_boundaries (
   id SERIAL,
-  object_id VARCHAR,
+  object_id INT,
   wkb_geometry GEOMETRY(GEOMETRY, 4326),
   PRIMARY KEY(id)
 );
 
 
 -- GEOMETRY INDEX
-CREATE UNIQUE INDEX IF NOT EXISTS monument_boundaries_id_idx ON monument_boundaries (id);
-CREATE INDEX IF NOT EXISTS monument_boundaries_wkb_geometry_geom_idx ON monument_boundaries USING gist (wkb_geometry);
+CREATE INDEX IF NOT EXISTS monument_boundaries_object_id_idx ON monument_boundaries (object_id);
+CREATE INDEX IF NOT EXISTS monument_boundaries_wkb_geometry_idx ON monument_boundaries USING GIST (wkb_geometry);
+CREATE UNIQUE INDEX IF NOT EXISTS monument_boundaries_object_id_wkb_geometry_hash_idx ON monument_boundaries (object_id, (md5(wkb_geometry::text)));
